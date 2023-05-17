e7a662dd27a139514c5dc76917d9baf77a39ef25
Dung.ND <94594983+vuhoangdungnd@users.noreply.github.com>
2023-05-17 23:54:40 +0700

    NodeJs #3

--- src/index.js
+++ src/index.js
@@ -7,6 +7,13 @@ const port = 3020;
 
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.use(
+    express.urlencoded({
+        extended: true,
+    }),
+);
+app.use(express.json());
+
 //HTTP logger
 app.use(morgan('combined'));
 
@@ -29,6 +36,14 @@ app.get('/news', (req, res) => {
     res.render('news');
 });
 
+app.get('/search', (req, res) => {
+    res.render('search');
+});
+
+app.post('/search', (req, res) => {
+    console.log(req.body);
+    res.send();
+});
 app.listen(port, () => {
     console.log(`Example app listening on port http://localhost:${port}`);
 });
