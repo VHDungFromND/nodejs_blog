3508e3502fc3c247e85d04c762a826dba91ca0cc
Dung.ND <94594983+vuhoangdungnd@users.noreply.github.com>
2023-05-18 23:15:16 +0700

    NodeJs #4

--- src/index.js
+++ src/index.js
@@ -5,6 +5,8 @@ const path = require('path');
 const app = express();
 const port = 3020;
 
+const route = require('./routes');
+
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use(
@@ -28,22 +30,8 @@ app.set('view engine', 'hbs');
 
 app.set('views', path.join(__dirname, 'resources', 'views'));
 
-app.get('/', (req, res) => {
-    res.render('home');
-});
+route(app);
 
-app.get('/news', (req, res) => {
-    res.render('news');
-});
-
-app.get('/search', (req, res) => {
-    res.render('search');
-});
-
-app.post('/search', (req, res) => {
-    console.log(req.body);
-    res.send();
-});
 app.listen(port, () => {
     console.log(`Example app listening on port http://localhost:${port}`);
 });
