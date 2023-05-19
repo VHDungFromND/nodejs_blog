51165d756d9117f18018a487f2cce8091197b0d9
Dung.ND <94594983+vuhoangdungnd@users.noreply.github.com>
2023-05-19 22:26:33 +0700

    NodeJs #6
    Delete course

--- src/config/db/index.js
+++ src/config/db/index.js
@@ -2,7 +2,10 @@ const mongoose = require('mongoose');
 
 async function connect() {
     try {
-        await mongoose.connect('mongodb://127.0.0.1:27017/education_dev');
+        await mongoose.connect('mongodb://127.0.0.1:27017/education_dev', {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+        });
         console.log('connection successful');
     } catch (error) {
         console.log('connection error');
